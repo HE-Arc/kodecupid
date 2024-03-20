a80295efd362941cc46dfdc219e78e36bac2c363
Thombrix <bruno.thomas-caruso@outlook.com>
2024-03-20 16:44:39 +0100

    Added view to register user

--- backend/kodecupid/urls.py
+++ backend/kodecupid/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from kodecupidapp.views import RegisterView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -25,4 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/user/register/', RegisterView.as_view(), name='register'),
 ]
