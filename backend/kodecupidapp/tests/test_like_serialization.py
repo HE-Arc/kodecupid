0e47b1877f437714f6386f63530eea073a4e6761
Thombrix <bruno.thomas-caruso@outlook.com>
2024-03-20 21:00:44 +0100

    updated like serializer test to user the source user id from fake request

--- backend/kodecupidapp/tests/test_like_serialization.py
+++ backend/kodecupidapp/tests/test_like_serialization.py
@@ -1,5 +1,5 @@
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.test import TestCase, RequestFactory
 from ..models import Like
 from ..serializers import LikeSerializer
 
@@ -9,13 +9,18 @@ class LikeSerializerTestCase(TestCase):
     def setUp(self):
         self.user1 = User.objects.create_user(username='user1', password='testpass123')
         self.user2 = User.objects.create_user(username='user2', password='testpass123')
+        self.factory = RequestFactory()
 
     def test_serializer_with_valid_data(self):
+        request = self.factory.post('/fake-url')
+        request.user = self.user1
+
         data = {
-            'source_user_id': self.user1.pk,
             'target_user_id': self.user2.pk,
         }
-        serializer = LikeSerializer(data=data)
+
+        serializer = LikeSerializer(data=data, context={'request': request})
+        
         self.assertTrue(serializer.is_valid())
         like = serializer.save()
         self.assertEqual(Like.objects.count(), 1)
@@ -23,6 +28,11 @@ class LikeSerializerTestCase(TestCase):
         self.assertEqual(like.target_user, self.user2)
 
     def test_serializer_with_invalid_data(self):
-        data = {'source_user_id': 999, 'target_user_id': 998}
-        serializer = LikeSerializer(data=data)
+        request = self.factory.post('/another-fake-url')
+        request.user = self.user1
+
+        data = {
+            # empty to simulate missing data
+        }
+        serializer = LikeSerializer(data=data, context={'request': request})
         self.assertFalse(serializer.is_valid())
