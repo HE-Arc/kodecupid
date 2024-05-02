912eb686742444d12fac2bf3c42c411147043e49
Lucas Gosteli <lucas.gosteli@he-arc.ch>
2024-05-02 19:46:25 +0200

    fix permision

--- deploy.sh
+++ deploy.sh
@@ -19,15 +19,15 @@ rm -f Pipfile.lock
 rm -f requirement.txt
 rm -rf static/*
 
-pipenv install --deploy
-pipenv run python3 manage.py migrate
+sudo pipenv install --deploy
+sudo pipenv run python3 manage.py migrate
 
-pipenv run python3 manage.py import_tags
+sudo pipenv run python3 manage.py import_tags
 
-pipenv run python3 manage.py spectacular --file schema.yml
-pipenv run python3 manage.py collectstatic --noinput
+sudo pipenv run python3 manage.py spectacular --file schema.yml
+sudo pipenv run python3 manage.py collectstatic --noinput
 
-pipenv run pip freeze > requirement.txt
+sudo pipenv run pip freeze > requirement.txt
 pip install -r requirement.txt
 
 cd "$root"
